8ae23491b60e883fef18ef7c4ede7b8b9f41bdf8
James Ots <code@jamesots.com>
2013-06-23 11:09:11 +0100

    Put actual tests in the test file

--- test/entity_test.dart
+++ test/entity_test.dart
@@ -5,6 +5,7 @@ import 'package:sqljocky/sqljocky.dart';
 import 'package:options_file/options_file.dart';
 import 'package:sqljocky/utils.dart';
 import 'package:logging/logging.dart';
+import 'package:unittest/unittest.dart';
 import 'dart:async';
 
 @Entity(table: "thing")
@@ -65,7 +66,7 @@ Future setup(ConnectionPool pool) {
   Completer completer = new Completer();
   
   var dropper = new TableDropper(pool, ['thing', 'SomethingElse']);
-  dropper.dropTables().then((x) {
+  dropper.dropTables().then((_) {
     var creator = new QueryRunner(pool, ['''create table thing (
                                                 name varchar(255), 
                                                 userId varchar(255),
@@ -79,7 +80,7 @@ Future setup(ConnectionPool pool) {
                                                 wibble varchar(255), 
                                                 primary key (id)
                                                 )''']);
-    creator.executeQueries().then((y) {
+    creator.executeQueries().then((_) {
       completer.complete(null);
     });
   });
@@ -94,58 +95,147 @@ void main() {
     print("${r.time}: ${r.loggerName}: ${r.message}");
   });
 
-  
-  var thingDao;
-  var somethingElseDao;
-  var thing = new Thing("jamesots", "James", 36);
-  var it1 = new SomethingElse(null, 123, "blah");
-  var it2 = new SomethingElse(null, -3, "what?");
-  var thing2;
-  print("starting");
   var connectionDetails = getConnectionDetails();
   var pool = new ConnectionPool(user:connectionDetails.user, password:connectionDetails.password, 
       port:connectionDetails.port, db:connectionDetails.db, 
       host:connectionDetails.host, max: 5);
 
-  thingDao = new ThingDao(pool);
-  somethingElseDao = new EntityDao<SomethingElse>(SomethingElse, pool);
-  setup(pool).then((_) {
-    return thingDao.insertNew(thing);
-  }).then((_) {
-    return somethingElseDao.insertNew(it1);
-  }).then((_) {
-    return somethingElseDao.insertNew(it2);
-  }).then((_) {
-    print("inserted");
-    return thingDao.update(thing);
-  }).then((_) {
-    print("updated");
-    thing2 = thingDao.fromJson('{"userId":"jonny", "name":"Jon","age":5}');
-    return thingDao.insertNew(thing2);
-  }).then((_) {
-    print("inserted");
-    return thingDao.delete(thing2);
-  }).then((_) {
-    print("deleted");
-    return thingDao.readAll();
-  }).then((list) {
-    print("read all");
-    list.forEach((item) {
-      print(thingDao.toJson(item));
+  var thingDao = new ThingDao(pool);
+  var somethingElseDao = new EntityDao<SomethingElse>(SomethingElse, pool);
+
+  test('setup', () {
+    return setup(pool);
+  });
+  
+  test('can insert thing', () {
+    var c = new Completer();
+    var thing = new Thing("jamesots", "James", 36);
+    thingDao.insertNew(thing).then((_) {
+      return pool.query("select * from thing");
+    }).then((result) {
+      return result.stream.toList();
+    }).then((list) {
+      expect(list.length, equals(1));
+      expect(list[0], equals(["James", "jamesots", 36]));
+      c.complete();
     });
-    return somethingElseDao.readAll();
-  }).then((list) {
-    print("read all something elses:");
-    list.forEach((item) {
-      print(somethingElseDao.toJson(item));
+    return c.future;
+  });
+
+  test('update thing', () {
+    var c = new Completer();
+    var thing = new Thing("jamesots", "Bill", 100);
+    thingDao.update(thing).then((_) {
+      return pool.query("select * from thing");
+    }).then((result) {
+      return result.stream.toList();
+    }).then((list) {
+      expect(list.length, equals(1));
+      expect(list[0], equals(["Bill", "jamesots", 100]));
+      c.complete();
     });
-    return thingDao.read("jamesots");
-  }).then((list) {
-    print("read item");
-    list.forEach((item) {
-      print(thingDao.toJson(item));
+    return c.future;
+  });
+
+  test('insert it1', () {
+    var c = new Completer();
+    var it1 = new SomethingElse(null, 123, "blah");
+    somethingElseDao.insertNew(it1).then((_) {
+      return pool.query("select * from SomethingElse");
+    }).then((result) {
+      return result.stream.toList();
+    }).then((list) {
+      expect(list.length, equals(1));
+      expect(list[0], equals([1, 123, 'blah']));
+      c.complete();
     });
+    return c.future;
+  });
+
+  test('insert it2', () {
+    var c = new Completer();
+    var it2 = new SomethingElse(null, -3, "what?");
+    somethingElseDao.insertNew(it2).then((_) {
+      return pool.query("select * from SomethingElse order by id");
+    }).then((result) {
+      return result.stream.toList();
+    }).then((list) {
+      expect(list.length, equals(2));
+      expect(list[1], equals([2, -3, 'what?']));
+      c.complete();
+    });
+    return c.future;
+  });
+
+  test('insert thing2', () {
+    var c = new Completer();
+    var thing2 = thingDao.fromJson('{"userId":"jonny", "name":"Jon","age":5}');
+    thingDao.insertNew(thing2).then((_) {
+      return pool.query("select * from thing where userId = 'jonny'");
+    }).then((result) {
+      return result.stream.toList();
+    }).then((list) {
+      expect(list.length, equals(1));
+      expect(list[0], equals(["Jon", "jonny", 5]));
+      c.complete();
+    });
+    return c.future;
+  });
+
+  test('delete thing2', () {
+    var c = new Completer();
+    var thing2 = thingDao.fromJson('{"userId":"jonny", "name":"Jon","age":5}');
+    thingDao.delete(thing2).then((_) {
+      return pool.query("select * from thing where userId = 'jonny'");
+    }).then((result) {
+      return result.stream.toList();
+    }).then((list) {
+      expect(list.length, equals(0));
+      c.complete();
+    });
+    return c.future;
+  });
+
+  test('read all things', () {
+    var c = new Completer();
+    thingDao.readAll().then((list) {
+      expect(list.length, equals(1));
+      expect(list[0].userId, equals('jamesots'));
+      expect(list[0].name, equals('Bill'));
+      expect(list[0].age, equals(100));
+      c.complete();
+    });
+    return c.future;
+  });
+
+  test('read all something elses', () {
+    var c = new Completer();
+    somethingElseDao.readAll().then((list) {
+      expect(list.length, equals(2));
+      expect(list[0].id, equals(1));
+      expect(list[0].bobId, equals(123));
+      expect(list[0].wibble, equals('blah'));
+      expect(list[1].id, equals(2));
+      expect(list[1].bobId, equals(-3));
+      expect(list[1].wibble, equals('what?'));
+      c.complete();
+    });
+    return c.future;
+  });
+
+  test('read thing', () {
+    var c = new Completer();
+    thingDao.read("jamesots").then((list) {
+      expect(list.length, equals(1)); // maybe read should throw error if more than one returned?
+      expect(list[0].userId, equals('jamesots'));
+      expect(list[0].name, equals('Bill'));
+      expect(list[0].age, equals(100));
+      c.complete();
+    });
+    return c.future;
+  });
+  
+  test('close', () {
     pool.close();
-    print("finished");
   });
 }
